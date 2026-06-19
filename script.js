cade0cd143d733725a82444f63cd84948ef41972
brokensoulsandbackroads <brokensoulsandbackroads@gmail.com>
2026-06-19 13:18:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 document.getElementById("year").textContent = new Date().getFullYear();
+
+function playSong(videoId, title){
+  const player = document.getElementById("music-player");
+  const frame = document.getElementById("player-frame");
+  const titleBox = document.getElementById("player-title");
+
+  titleBox.textContent = title;
+
+  frame.src = "https://www.youtube.com/embed/" + videoId + "?autoplay=1";
+
+  player.classList.add("open");
+}
+
+function closePlayer(){
+  const player = document.getElementById("music-player");
+  const frame = document.getElementById("player-frame");
+
+  player.classList.remove("open");
+  frame.src = "";
+}
